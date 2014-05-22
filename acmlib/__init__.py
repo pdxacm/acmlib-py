f80a80183157e467ed01037dee25116965fbedb8
Cameron Brandon White <cameronbwhite90@gmail.com>
2014-05-22 06:17:58 -0700

    Added install scripts

---
+++ acmlib/__init__.py
@@ -0,0 +1,6 @@
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
+DATE_FORMAT = '%Y-%m-%d'
+
+DEFAULT_BASE_URL = "http://www.acm.pdx.edu"
+
+from .main import AcmLib
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+#Copyright (C) 2014, Cameron Brandon White
+# -*- coding: utf-8 -*-
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='acmlib',
+        version='0.1.0',
+        description='library for the acm',
+        author='Cameron Brandon White',
+        author_email='cameronbwhite90@gmail.com',
+        provides=[
+            'acmlib',
+        ],
+        packages=[
+            "acmlib",
+        ],
+        install_requires = [
+            'characteristic',
+            'requests',
+        ],
+        include_package_data=True,
+    )
